056a2119e147a02b9a4419b79638aacb22f96309
elij <elij.mx@gmail.com>
2012-07-21 21:47:16 -0700

    update code to be more testable, add some tests

--- statsd/main.go
+++ statsd/main.go
@@ -8,46 +8,71 @@ import (
 )
 
 type StatsdClient struct {
-	addr *net.UDPAddr
-	conn *net.UDPConn
-	prefx string
+	// connection of type interface net.Conn
+	conn net.Conn
+	// prefix for statsd name
+	prefix string
 }
 
+
+// Increments a statsd count type
+// stat is a string name for the metric
+// value is the integer value
+// rate is the sample rate (0.0 to 1.0)
 func (s *StatsdClient) Inc(stat string, value int64, rate float32) error {
 	dap := fmt.Sprintf("%d|c", value)
 	return s.submit(stat, dap, rate)
 }
 
+// Decrements a statsd count type
+// stat is a string name for the metric
+// value is the integer value
+// rate is the sample rate (0.0 to 1.0)
 func (s *StatsdClient) Dec(stat string, value int64, rate float32) error {
 	return s.Inc(stat, -value, rate)
 }
 
+// Submits/Updates a statsd guage type
+// stat is a string name for the metric
+// value is the integer value
+// rate is the sample rate (0.0 to 1.0)
 func (s *StatsdClient) Guage(stat string, value int64, rate float32) error {
 	dap := fmt.Sprintf("%d|g", value)
 	return s.submit(stat, dap, rate)
 }
 
+// Submits a statsd timing type
+// stat is a string name for the metric
+// value is the integer value
+// rate is the sample rate (0.0 to 1.0)
 func (s *StatsdClient) Timing(stat string, delta int64, rate float32) error {
 	dap := fmt.Sprintf("%d|ms", delta)
 	return s.submit(stat, dap, rate)
 }
 
+// Sets/Updates the statsd client prefix
+func (s *StatsdClient) SetPrefix(prefix string) {
+	s.prefix = prefix
+}
+
+// submit formats the statsd event data, handles sampling, and prepares it,
+// and sends it to the server.
 func (s *StatsdClient) submit(stat string, value string, rate float32) error {
 	if rate < 1 {
 		if rand.Float32() < rate {
-			value = fmt.Sprintf("%s|@%4.3", value, rate)
+			value = fmt.Sprintf("%s|@%f", value, rate)
 		} else {
 			return nil
 		}
 	}
 
-	if s.prefx != "" {
-		stat = fmt.Sprintf("%s.%s", s.prefx, stat)
+	if s.prefix != "" {
+		stat = fmt.Sprintf("%s.%s", s.prefix, stat)
 	}
 
 	data := fmt.Sprintf("%s:%s", stat, value)
 
-	i, err := s.conn.Write([]byte(data))
+	i, err := s.send([]byte(data))
 	if err != nil {
 		return err
 	}
@@ -57,6 +82,16 @@ func (s *StatsdClient) submit(stat string, value string, rate float32) error {
 	return nil
 }
 
+// sends the data to the server endpoint over the net.Conn
+func (s *StatsdClient) send(data []byte) (int, error) {
+	i, err := s.conn.Write([]byte(data))
+	return i, err
+}
+
+// Returns a pointer to a new StatsdClient
+// addr is a string of the format "hostname:port", and must be parsable by
+// net.ResolveUDPAddr.
+// prefix is the statsd client prefix. Can be "" if no prefix is desired.
 func New(addr string, prefix string) (*StatsdClient, error) {
 	udpaddr, err := net.ResolveUDPAddr("udp", addr)
 	if err != nil {
@@ -69,9 +104,8 @@ func New(addr string, prefix string) (*StatsdClient, error) {
 	}
 
 	client := &StatsdClient{
-		addr: udpaddr,
 		conn: conn,
-		prefx: prefix}
+		prefix: prefix}
 
 	return client, nil
 }
---
+++ statsd/main_test.go
@@ -0,0 +1,122 @@
+package statsd
+
+import (
+	"testing"
+	"bytes"
+	"net"
+	//"fmt"
+)
+
+type fakeUDPConn struct {
+	*net.UDPConn
+	buf bytes.Buffer
+}
+
+func (f *fakeUDPConn) GetBuff() string {
+	res := f.buf.String()
+	f.buf.Reset()
+	return res
+}
+
+func (f *fakeUDPConn) Write(b []byte) (int, error) {
+	i, err := f.buf.Write(b)
+	return i, err
+}
+
+
+func TestGuage(t *testing.T) {
+	u := &fakeUDPConn{}
+	f := &StatsdClient{conn: u, prefix: "test"}
+
+	err := f.Guage("guage", 1, 1.0)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	b := u.GetBuff()
+	expected := "test.guage:1|g"
+	if b != expected {
+		t.Fatalf("got '%s' expected '%s'", b, expected)
+	}
+}
+
+func TestIncRatio(t *testing.T) {
+	u := &fakeUDPConn{}
+	f := &StatsdClient{conn: u, prefix: "test"}
+
+	err := f.Inc("count", 1, 0.999999)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	b := u.GetBuff()
+	expected := "test.count:1|c|@0.999999"
+	if b != expected {
+		t.Fatalf("got '%s' expected '%s'", b, expected)
+	}
+}
+
+func TestInc(t *testing.T) {
+	u := &fakeUDPConn{}
+	f := &StatsdClient{conn: u, prefix: "test"}
+
+	err := f.Inc("count", 1, 1.0)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	b := u.GetBuff()
+	expected := "test.count:1|c"
+	if b != expected {
+		t.Fatalf("got '%s' expected '%s'", b, expected)
+	}
+}
+
+func TestDec(t *testing.T) {
+	u := &fakeUDPConn{}
+	f := &StatsdClient{conn: u, prefix: "test"}
+
+	err := f.Dec("count", 1, 1.0)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	b := u.GetBuff()
+	expected := "test.count:-1|c"
+	if b != expected {
+		t.Fatalf("got '%s' expected '%s'", b, expected)
+	}
+}
+
+func TestTiming(t *testing.T) {
+	u := &fakeUDPConn{}
+	f := &StatsdClient{conn: u, prefix: "test"}
+
+	err := f.Timing("timing", 1, 1.0)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	b := u.GetBuff()
+	expected := "test.timing:1|ms"
+	if b != expected {
+		t.Fatalf("got '%s' expected '%s'", b, expected)
+	}
+}
+
+func TestEmptyPrefix(t *testing.T) {
+	u := &fakeUDPConn{}
+	f := &StatsdClient{conn: u}
+
+	err := f.Inc("count", 1, 1.0)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	b := u.GetBuff()
+	expected := "count:1|c"
+	if b != expected {
+		t.Fatalf("got '%s' expected '%s'", b, expected)
+	}
+}
+
