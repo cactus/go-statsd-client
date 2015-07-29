e40b7638f7685a86f743d77d78ca09a71b755b5c
elij <elij@wundrd.net>
2015-07-28 20:39:33 -0700

    more file renaming

--- statsd/client.go
+++ statsd/client.go
@@ -2,9 +2,7 @@ package statsd
 
 import (
 	"bytes"
-	"errors"
 	"math/rand"
-	"net"
 	"strconv"
 	"sync"
 	"time"
@@ -36,11 +34,6 @@ type Statter interface {
 	SetInt(string, int64, float32) error
 	Raw(string, string, float32) error
 	SetPrefix(string)
-	Close() error
-}
-
-type Sender interface {
-	Send(data []byte) (int, error)
 	Close() error
 }
 
@@ -225,57 +218,6 @@ func (s *Client) SetPrefix(prefix string) {
 		return
 	}
 	s.prefix = prefix
-}
-
-// SimpleSender provides a socket send interface.
-type SimpleSender struct {
-	// underlying connection
-	c net.PacketConn
-	// resolved udp address
-	ra *net.UDPAddr
-}
-
-// Send sends the data to the server endpoint.
-func (s *SimpleSender) Send(data []byte) (int, error) {
-	// no need for locking here, as the underlying fdNet
-	// already serialized writes
-	n, err := s.c.(*net.UDPConn).WriteToUDP(data, s.ra)
-	if err != nil {
-		return 0, err
-	}
-	if n == 0 {
-		return n, errors.New("Wrote no bytes")
-	}
-	return n, nil
-}
-
-// Closes SimpleSender
-func (s *SimpleSender) Close() error {
-	err := s.c.Close()
-	return err
-}
-
-// Returns a new SimpleSender for sending to the supplied addresss.
-//
-// addr is a string of the format "hostname:port", and must be parsable by
-// net.ResolveUDPAddr.
-func NewSimpleSender(addr string) (Sender, error) {
-	c, err := net.ListenPacket("udp", ":0")
-	if err != nil {
-		return nil, err
-	}
-
-	ra, err := net.ResolveUDPAddr("udp", addr)
-	if err != nil {
-		return nil, err
-	}
-
-	sender := &SimpleSender{
-		c:  c,
-		ra: ra,
-	}
-
-	return sender, nil
 }
 
 // Returns a pointer to a new Client, and an error.
