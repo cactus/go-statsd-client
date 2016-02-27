8553df962331209f518c579302925cec5e33cda4
elij <elij@wundrd.net>
2016-02-27 12:28:10 -0800

    add helper stat validation func

---
+++ statsd/validator.go
@@ -0,0 +1,24 @@
+// Copyright (c) 2012-2016 Eli Janssen
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file.
+
+package statsd
+
+import (
+	"fmt"
+	"regexp"
+)
+
+type ValidatorFunc func(string) error
+
+var safeName = regexp.MustCompile(`^[a-zA-Z0-9\-_.]+$`)
+
+// A stat name validator function. This function may be used to validate
+// whether a stat name contains invalid characters. If invalid
+// characters are found, the function will return an error.
+func SafeNameValidator(stat string) error {
+	if !safeName.MatchString(stat) {
+		return fmt.Errorf("invalid stat name: %s", stat)
+	}
+	return nil
+}
